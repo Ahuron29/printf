39ddca1c831b68f48acb6d0ca237d916dacdb553
Acquah Kingsley Segu <ksegu.acq@icloud.com>
2022-04-20 04:24:07 +0200

    Another algo direction

--- _printf.c
+++ _printf.c
@@ -10,9 +10,9 @@
  */
 int printIdentifiers(char next, va_list arg)
 {
-	int functsIndex;
+  int functsIndex;
 
-	identifierStruct functs[] = {
+  identifierStruct functs[] = {
 			       {"c", print_char},
 			       {"s", print_str},
 			       {"d", print_int},
@@ -24,16 +24,15 @@ int printIdentifiers(char next, va_list arg)
 			       {"X", print_HEX},
 			       {"S", print_STR},
 			       {NULL, NULL}
-	};
+  };
 
-	for (functsIndex = 0; functs[functsIndex].indentifier != NULL; functsIndex++)
-	{
-		if (functs[functsIndex].indentifier[0] == next)
-			return (functs[functsIndex].printer(arg));
-	}
-	return (0);
+  for (functsIndex = 0; functs[functsIndex].indentifier != NULL; functsIndex++)
+    {
+      if (functs[functsIndex].indentifier[0] == next)
+	return (functs[functsIndex].printer(arg));
+    }
+  return (0);
 }
-
 /**
  * _printf - mimic printf from stdio
  * Description: produces output according to a format
@@ -45,41 +44,44 @@ int printIdentifiers(char next, va_list arg)
  */
 int _printf(const char *format, ...)
 {
-	unsigned int i;
-	int identifierPrinted = 0, charPrinted = 0;
-	va_list arg;
+  unsigned int i;
+  int identifierPrinted = 0, charPrinted = 0;
+  va_list arg;
 
-	va_start(arg, format);
-	if (format == NULL)
-		return (-1);
-	for (i = 0; format[i] != '\0'; i++)
+  va_start(arg, format);
+  if (format == NULL)
+    return (-1);
+
+  for (i = 0; format[i] != '\0'; i++)
+    {
+      if (format[i] != '%')
 	{
-		if (format[i] != '%')
-		{
-			_putchar(format[i]);
-			charPrinted++;
-			continue;
-		}
-		if (format[i + 1] == '%')
-		{
-			_putchar('%');
-			charPrinted++;
-			i++;
-			continue;
-		}
-		if (format[i + 1] == '\0')
-			return (-1);
-		identifierPrinted = printIdentifiers(format[i + 1], arg);
-		if (identifierPrinted == -1 || identifierPrinted != 0)
-			i++;
-		if (identifierPrinted > 0)
-			charPrinted += identifierPrinted;
-		if (identifierPrinted == 0)
-		{
-			_putchar('%');
-			charPrinted++;
-		}
+	  _putchar(format[i]);
+	  charPrinted++;
+	  continue;
 	}
-	va_end(arg);
-	return (charPrinted);
+      if (format[i + 1] == '%')
+	{
+	  _putchar('%');
+	  charPrinted++;
+	  i++;
+	  continue;
+	}
+      if (format[i + 1] == '\0')
+	return (-1);
+
+      identifierPrinted = printIdentifiers(format[i + 1], arg);
+      if (identifierPrinted == -1 || identifierPrinted != 0)
+	i++;
+      if (identifierPrinted > 0)
+	charPrinted += identifierPrinted;
+
+      if (identifierPrinted == 0)
+	{
+	  _putchar('%');
+	  charPrinted++;
+	}
+    }
+  va_end(arg);
+  return (charPrinted);
 }
--- print_binary.c
+++ print_binary.c
@@ -8,10 +8,10 @@
 
 void print_binary(unsigned int n, unsigned int *printed)
 {
-	if  (n > 1)
-	{
-		*printed += 1;
-		print_binary(n >> 1, printed);
-	}
-	_putchar((n & 1) + '0');
+  if  (n > 1)
+    {
+      *printed += 1;
+      print_binary(n >> 1, printed);
+    }
+  _putchar((n & 1) + '0');
 }
