b235cbd8fd2ad53982d4d63e765434c5c6a7c50c
Emmanuel Ahuron <65813159+Ahuron29@users.noreply.github.com>
2022-04-22 05:05:09 +0000

    Function to convert numbers

---
+++ converter.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * convert - this converts number and base into string
+ * @num: the input number
+ * @base: the input base
+ * @lowercase: flag if hexa values need to be lowercase
+ * Return: result string
+ */
+char *convert(unsigned long int num, int base, int lowercase)
+{
+	static char *rep;
+	static char buffer[50];
+	char *ptr;
+
+	rep = (lowercase)
+		? "0123456789abcdef"
+		: "0123456789ABCDEF";
+	ptr = &buffer[49];
+	*ptr = '\0';
+	do {
+		*--ptr = rep[num % base];
+		num /= base;
+	} while (num != 0);
+
+	return (ptr);
+}
